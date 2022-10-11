4147e266e14e2c6f7fdc0465f4d1f3e60318b1a2
Yasir Ali <sdr.yasir@gmail.com>
2022-10-11 02:53:29 -0700

    Added counter example

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import Header from "./components/Header";
 import Main from './components/Main'
 import Footer from './components/Footer'
+import { useState } from "react";
 
 function App() {
+
+
+  let [num, setNum] = useState(101)
+
+  let handleIncrement = () => {
+    if(num>=100) return;
+    setNum(num+1)
+  }
+  let handleDecrement = () => {
+    if(num<=80) return;
+    setNum(num-1)
+  }
+
   return (
     <>
-      <Header/>
-      <Main/>
-      <Footer/>
+      <div>
+        <p>{num}</p>
+        <button disabled={num>=100 ? true : false} onClick={handleIncrement}>+</button>
+        <button disabled={num<=80 ? true : false} onClick={handleDecrement}>-</button>
+      </div>
     </>
   );
 }
